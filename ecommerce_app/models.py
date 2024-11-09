c6933c09ad896c05fc65a2ad7a5b6efd77724910
Muhammed Mijwad <mijuzz007@gmail.com>
2024-11-09 13:29:32 +0300

    added message field to request model

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -522,11 +522,12 @@ class ProductReturnRequest(models.Model):
         ('REJECTED', 'Rejected'),
     ]
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE, null=True, blank=True)
     reason = models.TextField()
-    refund_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    refund_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     status = models.CharField(max_length=20, choices=RETURN_STATUS_CHOICES, default='PENDING')
     requested_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"Return Request for {self.order_item.product.name} by {self.user.username}"
