22a2cd35a8388db34ffa2c77bdb2d3c8db7f96ce
Muhammed Mijwad <mijuzz007@gmail.com>
2024-11-14 21:58:33 +0300

    deploy

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-x3rmf3f_*&92^rmvusayo*81n5obt9x5xw&zeb02rpnpyj71yw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
