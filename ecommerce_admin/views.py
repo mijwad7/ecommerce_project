825261447c04c4a107882793f46f85cfc0923a13
Muhammed Mijwad <mijuzz007@gmail.com>
2024-11-19 15:45:57 +0300

    fixed admin issue

--- ecommerce_admin/views.py
+++ ecommerce_admin/views.py
@@ -30,7 +30,7 @@ def superuser_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.is_superuser:
             return view_func(request, *args, **kwargs)
-        return HttpResponseForbidden("You do not have permission to access this page.")
+        return redirect("login")
 
     return _wrapped_view
 
