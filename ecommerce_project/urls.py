417d6776050a2b08494fd73538c1889b682d8118
Muhammed Mijwad <mijuzz007@gmail.com>
2024-11-21 08:43:08 +0300

    fixed it

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
