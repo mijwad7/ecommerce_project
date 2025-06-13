d1e7e2b4a7aa492fd708e1156386ad6fee210529
Muhammed Mijwad <mijuzz007@gmail.com>
2025-06-13 15:09:08 +0530

    static files url fix

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -181,8 +181,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Separate directory for collected files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Source static files
+]
 
 
 MEDIA_URL = "/media/"
